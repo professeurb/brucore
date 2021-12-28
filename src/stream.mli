a7b16e92baeb9f9a2d309eec9d054ec2029b48b0
professeurb <olibrunet@free.fr>
2021-12-28 10:32:06 +0100

    Fixing stream.mli signature, again and again

--- src/stream.mli
+++ src/stream.mli
@@ -1,5 +1,7 @@
 include module type of struct include Stdlib.Stream end
 
+type 'a t = 'a Stdlib.Stream.t
+
 val of_file : string -> string Stream.t
 
 val map : ('a -> 'b) -> 'a Stream.t -> 'b Stream.t
